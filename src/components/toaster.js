0b35c4e62acc38e0c582dbfda41cbd3861817031
Toni Iltanen <toni.iltanen@codecontrol.fi>
2018-09-25 17:03:08 +0300

    toaster fix

--- src/components/toaster.js
+++ src/components/toaster.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 
 class Toaster extends Component {
     render(){
